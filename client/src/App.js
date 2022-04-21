30071e10d7dca0fcb5e755b8472e60ebbf33d99d
JavierRibaldelRio <javierribal@gmail.com>
2022-04-22 00:03:53 +0200

    He avanzado con el client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';     //Importa react router
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/api" element={<Main />} />
 
+      </Routes>
     </div>
   );
 }
---
+++ client/src/Main.js
@@ -0,0 +1,18 @@
+/**
+ * Javier Ribal del Río
+ * 
+ * javierribal@gmail.com
+ */
+
+import React from 'react';
+
+function Main() {
+    return (
+        <div>
+            <h1>Hola Soy la / aunque realmente me llamo main</h1>
+        </div>
+    );
+}
+
+
+export default Main;
