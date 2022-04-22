328d90ac97eb3a64ada6c4266e17b9a6829e706a
JavierRibaldelRio <javierribal@gmail.com>
2022-04-22 22:25:58 +0200

    He creado el esquema de moongose

---
+++ server/models/Deportiva.js
@@ -0,0 +1,13 @@
+const { Schema } = require('mongoose'); //Importa la libreria de Mongo
+
+//Creación del esquema
+
+var deportivasEsquema = new Schema({
+
+    id: { type: Number, required: true, uniqued: true },    //Id Ejemplo 1
+    marca: { type: String },                                //Nike
+    modelo: { type: String },                               //Nike 2000
+    observaciones: { type: String },                        //Son una edición limitada
+    talla: { type: Number },                                //41
+    fechaDeJubilación: { type: Date }
+});
