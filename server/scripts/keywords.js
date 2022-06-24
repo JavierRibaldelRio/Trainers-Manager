3c8bf18a07fc6f56f673b7eae8884faa067d92bf
JavierRibaldelRio <javierribal@gmail.com>
2022-06-24 19:09:59 +0200

    He añadido un script con el nombre de las varibles

---
+++ server/scripts/keywords.js
@@ -0,0 +1,17 @@
+/**
+ * Javier Ribal del Río
+ * 
+ * javierribal@gmail.com
+ */
+
+//Almacena los datos clave
+
+const TALLA = "talla";
+const MARCA = "marca";
+const MODELO = "modelo";
+const FECHADEJUBILACION = "fechaDeJubilacion";
+const OBSERVACIONES = "observaciones";
+const FECHACREACION = "fechaCreacion";
+const ULTIMOCAMBIO = "ultimoCambio";
+
+module.exports = { TALLA, MARCA, MODELO, FECHACREACION, FECHADEJUBILACION, OBSERVACIONES, ULTIMOCAMBIO };
