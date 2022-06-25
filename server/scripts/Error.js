54fa74a8d25fdebed49bf1a689344d7e494a97ab
JavierRibaldelRio <javierribal@gmail.com>
2022-06-25 13:25:05 +0200

    He creado un script que devuelve errores en el servidor

---
+++ server/scripts/Error.js
@@ -0,0 +1,18 @@
+/**
+ * Javier Ribal del Río
+ * 
+ * javierribal@gmail.com
+ */
+
+function errorServidor(err, res) {
+
+    res.status(500);
+
+    //Almacena el mensaje de error del Mongodb
+    const mensajeErr = 'Se ha producido un error en el servidor:  ' + err + "."
+    console.error(mensajeErr);
+
+    res.json(mensajeErr);
+}
+
+module.exports = errorServidor
