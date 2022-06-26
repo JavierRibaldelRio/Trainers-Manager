9e00534323d5e388f1bde2b2c9df32607dad4099
JavierRibaldelRio <javierribal@gmail.com>
2022-06-26 14:52:27 +0200

    Avances en la comunicación entre /buscar y /busqueda

--- client/src/Components/Buscar.js
+++ client/src/Components/Buscar.js
@@ -7,6 +7,7 @@
 import React, { Component } from 'react';
 import FormularioTrainer from './FormularioTrainer';
 import eliminarUndefined from '../Scripts/EliminarPropiedadesSinDefinir';
+import { useNavigate } from 'react-router-dom';
 
 class Buscar extends Component {
     constructor(props) {
@@ -22,7 +23,15 @@ class Buscar extends Component {
         //Del objeto entrante eliminamos los undefined
         eliminarUndefined(query);
 
-        console.log(query);
+        fetch('http://127.0.0.1:3001/search', 						//La url del sevidor
+            {
+                method: 'POST', // or 'PUT'
+                body: JSON.stringify(query), // data can be `string` or {object}!
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => response.json())
+            .then(respuesta => this.setState({ respuesta: respuesta }));
 
 
     }
@@ -30,8 +39,29 @@ class Buscar extends Component {
 
 
     render() {
+        //Comprueba si hay algo que mostrar
+        if (this.state.respuesta !== undefined) {
+            if (this.state.respuesta.length === 0) {
+
+                alert("No hay ninguna deportiva que cumpla todas las características");
+            }
+
+            else {
+                // Coge el navigate desde props
+                const { navigate } = this.props;
+
+                navigate('/busqueda', { state: { id: 1, name: 'sabaoon' } });
+
+            }
+        }
+
         return (<FormularioTrainer textoBoton={'Buscar'} ocultarObservaciones={true} enviar={this.buscar} />);
     }
 }
 
-export default Buscar;
+//Envolvemos el componente de clase en un componente de función para poder usar navigate
+export default function (props) {
+    const navigate = useNavigate();
+
+    return <Buscar {...props} navigate={navigate} />;
+}
---
+++ client/src/Components/Busqueda.js
@@ -0,0 +1,29 @@
+/**
+ * Javier Ribal del Río
+ * 
+ * javierribal@gmail.com
+ */
+
+import React, { Component } from 'react';
+
+import { useLocation } from 'react-router-dom';
+
+
+class Busqueda extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<p>Hola ::: {JSON.stringify(this.props.location.state.name)}</p>);
+    }
+}
+
+
+//Creamos el hook que nos poermite usar location
+export default function (props) {
+
+    const location = useLocation();
+
+    return <Busqueda {...props} location={location} />
+
+};
